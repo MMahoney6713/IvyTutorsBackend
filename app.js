7870a46674fd769ab88f145cceb13cdb0fbe2049
mmahoney6713 <mrm257@cornell.edu>
2022-03-18 23:06:26 -0700

    trying to fix bug

--- app.js
+++ app.js
@@ -20,20 +20,20 @@ const app = express();
 
 
 // Taken from https://www.codingdeft.com/posts/nodejs-react-cors-error/
-const domainsFromEnv = process.env.CORS_DOMAINS || "http://localhost:3000";
-const whitelist = domainsFromEnv.split(",").map(item => item.trim());
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
-
+// const domainsFromEnv = process.env.CORS_DOMAINS || "http://localhost:3000";
+// const whitelist = domainsFromEnv.split(",").map(item => item.trim());
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+// app.use(cors(corsOptions))
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan("tiny"));
